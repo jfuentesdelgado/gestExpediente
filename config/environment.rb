3b6c72b2f216becbd3d07d21a3b56ac44532fc97
Jose Fuentes Delgado <jfuetnes.jfd@gmail.com>
2014-01-10 10:27:51 +0100

    Preparacion para mercancias

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 GestExpediente::Application.initialize!
+
+# JFD Para que reconozca el plural de mercancia
+ActivoSupport::Inflector.inflections do |inflect|
+	inflect.irregular 'mercancia', 'mercancias'
+end
