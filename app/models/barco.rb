e07245c05fae4b93ddcc948ba114ac6884e5a63c
Angel <angellnieto@gmail.com>
2014-05-07 13:38:16 +0200

    Aspecto 2

--- app/models/barco.rb
+++ app/models/barco.rb
@@ -1,4 +1,11 @@
 class Barco < ActiveRecord::Base
 	has_many :registros
 	validates :nombre, :presence => true
+
+	before_destroy do |barco|
+		if barco.resgistros.any?
+			false
+		end
+	end
+
 end
